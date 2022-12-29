57b27d9491588d7054eacc84de968231680acd4e
Napoleón Pérez <87319901+napo178@users.noreply.github.com>
2022-12-29 11:01:17 -0600

    update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit   as st
-st.title('Negocios Energeticos App')
+import pandas as pd
+import pickle
+import numpy as np
+st.title('App de deteccion de fraudes')
 from PIL import Image
 image = Image.open('delsur.png')
 st.image(image, caption='Delsur')
 
-import streamlit as st
-import pandas as pd
-import pickle
-import numpy as np
+
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -17,21 +17,15 @@ if uploaded_file is not None:
     st.write(dataframe)
 
 
-st.subtitle('Upload file to predict')
-# features for prediction 
-# X = sales_join[['day', 'month', 'id','spree_store_id','total_sales']]
-#y = sales_join['items_total']
-
-# Add a heading for input features
-st.subheader('Enter  Features for Predictions')
-
 
 test=pd.read_csv('model')
+
+
 
 # Load  the model from disk
 
 if st.button("Predict"):
-    pickle_in = open('finalized_model.pkl', 'rb')
+    pickle_in = open('modelinicial.pkl', 'rb')
     model = pickle.load(pickle_in)
     predict=model.predict(test)
   
@@ -40,4 +34,7 @@ if st.button("Predict"):
      The number of items in the day is :  {predict} 
     """)    # Get the input features
     # run predictions
+
+
+
 
